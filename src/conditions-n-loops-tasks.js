917c5c3c34c2de71235c9418b64d815144e32197
Olga <olga.slepova87@gmail.com>
2025-11-15 14:10:02 +0100

    feat: implement fucion canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -64,8 +64,13 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const sameFile = queen.x === king.x;
+  const sameRank = queen.y === king.y;
+  const sameDiag =
+    queen.x - queen.y === king.x - king.y ||
+    queen.x + queen.y === king.x + king.y;
+  return sameFile || sameRank || sameDiag;
 }
 
 /**
